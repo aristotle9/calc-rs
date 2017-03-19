3038b771ed96825e2ae5876faf457225e7bcada9
aristotle9 <lanfan.1987@gmail.com>
2017-03-19 19:54:46 +0800

    output source files

---
+++ src/calc_lexer.rs
@@ -0,0 +1,352 @@
+extern crate rustc_serialize;
+
+use self::rustc_serialize::json::Json;
+
+use std::error::Error;
+use std::fmt;
+use std::u64;
+
+
+
+/**
+ * Created by as3cc on Sat Mar 18 16:26:03 GMT+0800 2017.
+ */
+type TransTable = Vec<RangeItem>;
+type Token = usize;
+type CalcLexerResult = Result<Token, CalcLexerError>;
+const DEADSTATE: usize = 0xffffffff;
+const END_TOKEN: usize = 0;
+
+
+#[derive(Debug)]
+pub struct CalcLexerError {
+    token_name: &'static str,
+    token_index: usize,
+    line: u64,
+    col: u64,
+    reason: String,
+}
+
+impl fmt::Display for CalcLexerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CalcLexerError: {} {}@row:{}col:{}", self.reason, self.token_name, self.line, self.col)
+    }
+}
+
+impl Error for CalcLexerError {
+    fn description(&self) -> &str {
+        &self.reason
+    }
+}
+
+#[derive(Debug)]
+struct StateTransItem {
+    is_dead: bool,
+    final_index: i64,
+    to_states: Vec<usize>,
+    trans_edge: TransTable,
+}
+
+#[derive(Debug)]
+struct RangeItem {
+    from: u64,
+    to: u64,
+    value: u64,
+}
+
+#[derive(Debug)]
+pub struct CalcLexer {
+    trans_table: Vec<StateTransItem>,
+    input_table: TransTable,
+    _start:  usize,
+    _old_start:  usize,
+    _token_index:  usize,
+    _yytext:  Json,
+    _yy:  usize,//&mut u8
+    _ended:  bool,
+    _initial_input:  usize,
+    _line:  u64,
+    _col:  u64,
+    _advanced:  bool,
+    _source:  Vec<char>,//
+}
+
+impl CalcLexer {
+    
+    pub fn find_token_name(token_index: usize) -> &'static str {
+        return match token_index {
+0 => "<$>",
+1 => "(",
+2 => ")",
+3 => "*",
+4 => "+",
+5 => "-",
+6 => "/",
+7 => "num",
+_ => "<undefined_token>",
+};
+    }
+
+    pub fn find_initial_name(initial_input: u64) -> &'static str {
+        return match initial_input {
+1 => "INITIAL",
+_ => "undefined initial input",
+};
+    }
+    
+    #[inline]
+    pub fn is_end_token(token: usize) -> bool {
+        token == END_TOKEN
+    }
+
+    pub fn new() -> CalcLexer {
+        ;let _trans_table = 
+vec![StateTransItem{is_dead:false, final_index:4294967295, to_states:vec![4294967295, 1], trans_edge:vec![RangeItem{from:0, to:8, value:0}, RangeItem{from:9, to:9, value:1}]}, StateTransItem{is_dead:false, final_index:4294967295, to_states:vec![4294967295, 9, 8, 7, 6, 5, 4, 3, 2], trans_edge:vec![RangeItem{from:0, to:0, value:0}, RangeItem{from:1, to:1, value:1}, RangeItem{from:2, to:2, value:2}, RangeItem{from:3, to:3, value:3}, RangeItem{from:4, to:4, value:4}, RangeItem{from:5, to:5, value:5}, RangeItem{from:6, to:6, value:6}, RangeItem{from:7, to:7, value:7}, RangeItem{from:8, to:8, value:8}, RangeItem{from:9, to:9, value:0}]}, StateTransItem{is_dead:false, final_index:0, to_states:vec![4294967295, 2], trans_edge:vec![RangeItem{from:0, to:7, value:0}, RangeItem{from:8, to:8, value:1}, RangeItem{from:9, to:9, value:0}]}, StateTransItem{is_dead:true, final_index:1, to_states:vec![], trans_edge:vec![]}, StateTransItem{is_dead:true, final_index:3, to_states:vec![], trans_edge:vec![]}, StateTransItem{is_dead:true, final_index:2, to_states:vec![], trans_edge:vec![]}, StateTransItem{is_dead:true, final_index:4, to_states:vec![], trans_edge:vec![]}, StateTransItem{is_dead:true, final_index:5, to_states:vec![], trans_edge:vec![]}, StateTransItem{is_dead:false, final_index:7, to_states:vec![4294967295, 8], trans_edge:vec![RangeItem{from:0, to:1, value:0}, RangeItem{from:2, to:2, value:1}, RangeItem{from:3, to:9, value:0}]}, StateTransItem{is_dead:true, final_index:6, to_states:vec![], trans_edge:vec![]}]
+;let _input_table = 
+vec![RangeItem{from:0, to:8, value:0}, RangeItem{from:9, to:10, value:2}, RangeItem{from:11, to:11, value:0}, RangeItem{from:12, to:13, value:2}, RangeItem{from:14, to:31, value:0}, RangeItem{from:32, to:32, value:2}, RangeItem{from:33, to:39, value:0}, RangeItem{from:40, to:40, value:3}, RangeItem{from:41, to:41, value:1}, RangeItem{from:42, to:42, value:6}, RangeItem{from:43, to:43, value:7}, RangeItem{from:44, to:44, value:0}, RangeItem{from:45, to:45, value:5}, RangeItem{from:46, to:46, value:0}, RangeItem{from:47, to:47, value:4}, RangeItem{from:48, to:57, value:8}, RangeItem{from:58, to:65535, value:0}]
+;
+        CalcLexer {
+            trans_table: _trans_table,
+            input_table: _input_table,
+            _start: 0,
+            _old_start: 0,
+            _token_index: 0,
+            _yytext: Json::Null,
+            _yy: 0,
+            _ended: false,
+            _initial_input: 1,
+            _line: 0,
+            _col: 0,
+            _advanced: true,
+            _source: vec![],
+        }
+    }
+
+    fn restart(&mut self, source: &str) {
+        self._ended = false;
+        self._start = 0;
+        self._old_start = 0;
+        self._line = 0;
+        self._col = 0;
+        self._advanced = true;
+        self._token_index = 0;
+        self._yytext = Json::Null;
+        self._yy = 0;
+        self._initial_input = 1;
+        self._source = source.chars().collect();
+        ;
+
+    }
+
+    #[inline]
+    pub fn set_source(&mut self, source: &str) {
+        self.restart(source);
+    }
+
+    #[inline]
+    pub fn advance(&mut self) {
+        self._advanced = true;
+    }
+
+    #[inline]
+    pub fn start_index(&self) -> usize {
+        self._old_start
+    }
+
+    #[inline]
+    pub fn end_index(&self) -> usize {
+        self._start
+    }
+
+    #[inline]
+    pub fn get_position(&self) -> [u64; 2] {
+        [self._line, self._col]
+    }
+
+    #[inline]
+    pub fn new_lexer_error(&self, reason: String) -> CalcLexerError {
+        // format!("{}@row:{}col:{}", Self::find_token_name(self._token_index), self._line, self._col)
+        CalcLexerError {
+            token_name: Self::find_token_name(self._token_index),
+            token_index: self._token_index,
+            line: self._line,
+            col: self._col,
+            reason: reason,
+        }
+    }
+
+    #[inline]
+    fn set_yytext(&mut self, value: Json) {
+        self._yytext = value;
+    }
+
+    #[inline]
+    pub fn get_yytext(&mut self) -> Json {
+        if self._yytext.is_null() && !self._ended {
+            self._yytext = Json::String(self._source[self.start_index() .. self.end_index()].iter().cloned().collect())
+        }
+        self._yytext.clone()
+    }
+
+    #[inline]
+    fn get_token_index(&self) -> usize {
+        self._token_index
+    }
+
+    #[inline]
+    fn begin(&mut self, initial_input: usize) {
+        self._initial_input = initial_input;
+    }
+
+    pub fn get_token(&mut self) -> CalcLexerResult {
+        if self._advanced {
+            match self.next() {
+                Ok(value) => {self._token_index = value;},
+                Err(err) => {return Err(err);},
+            };
+            self._advanced = false;
+        }
+        Ok(self._token_index)
+    }
+
+    fn next(&mut self) -> CalcLexerResult {
+        loop {
+            let mut _next_state: usize = 0;
+            let mut _ch: u64 = 0;
+            let mut _next: usize = self._start;
+            let mut _och: u64 = u64::MAX;
+            let mut _cur_state: usize = self.trans_table[0].to_states[self._initial_input];
+            let mut _last_final_state = DEADSTATE;
+            let mut _last_final_position = self._start;
+            loop {
+                if _next < self._source.len() {
+                    _ch = self._source[_next] as u64;
+                    /* 计算行列位置 */
+                    if _och != u64::MAX {
+                        if _ch == 0x0d {//\r
+                            self._col = 0;
+                            self._line += 1;
+                        } else if _ch == 0x0a {//\n
+                            if _och != 0x0d {//\r
+                                self._col = 0;
+                                self._line += 1;
+                            }
+                        } else {
+                            self._col += 1;
+                        }
+                    }
+
+                    _och = _ch;
+                    _next_state = match self.trans(_cur_state, _ch) {
+                        Ok(st) => { st },
+                        Err(err) => { return Err(err); }
+                    };
+                } else {
+                    _next_state = DEADSTATE;
+                }
+
+                if _next_state == DEADSTATE {
+                    if self._start == _last_final_position {
+                        if self._start == self._source.len() {
+                            if !self._ended {
+                                self._ended = true;
+                                return Ok(END_TOKEN);
+                            } else {
+                                return Err(self.new_lexer_error("aleady at end.".to_string()));
+                            }
+                        } else {
+                            return Err(self.new_lexer_error("invalid char".to_string()));
+                        }
+                    } else {
+                        self._yytext = Json::Null;//set yytext
+                        self._old_start = self._start;
+                        self._start = _last_final_position;
+                        let _findex = self.trans_table[_last_final_state].final_index;
+                        match _findex {
+0x0 => {
+    return Ok(7) /* num */ ;
+},
+0x1 => {
+    return Ok(4) /* + */ ;
+},
+0x2 => {
+    return Ok(5) /* - */ ;
+},
+0x3 => {
+    return Ok(3) /* * */ ;
+},
+0x4 => {
+    return Ok(6) /* / */ ;
+},
+0x5 => {
+    return Ok(1) /* ( */ ;
+},
+0x6 => {
+    return Ok(2) /* ) */ ;
+},
+_ => {},
+}
+                        break;
+                    }
+                } else {
+                    _next += 1;
+                    if self.trans_table[_next_state].final_index != 0xffffffff {
+                        _last_final_state = _next_state;
+                        _last_final_position = _next;
+                    }
+                    _cur_state = _next_state;
+                }
+            }
+        }
+    }
+
+    fn find(code: u64, table: &TransTable) -> u64 {
+        let mut max = table.len();
+        let mut min = 0;
+
+        loop {
+            let mid = (max + min) >> 1;
+            if table[mid].from <= code {
+                if table[mid].to >= code {
+                    return table[mid].value;
+                } else {
+                    min = mid + 1;
+                }
+            } else {
+                max = mid - 1;
+            }
+        }
+    }
+
+    fn trans(&self, cur_state: usize, ch: u64) -> CalcLexerResult {
+        if ch < self.input_table[0].from || ch > self.input_table[self.input_table.len() - 1].to {
+            return Err(self.new_lexer_error("input char out of valid range".to_string()));
+        }
+        if self.trans_table[cur_state].is_dead {
+            return Ok(DEADSTATE);
+        }
+        let pub_input = Self::find(ch, &self.input_table);
+        let inner_input = Self::find(pub_input, &self.trans_table[cur_state].trans_edge);
+        return Ok(self.trans_table[cur_state].to_states[inner_input as usize])
+    }
+
+    pub fn lex_seq(source: &str) -> Result<Vec<(&'static str, Token, Json, usize, usize)>, CalcLexerError> {//(token_name, token_index, yytext, star, end)
+        let mut lexer = CalcLexer::new();
+        lexer.set_source(source);
+        let mut tokens = Vec::new();
+        let mut token: usize = 0;
+        match lexer.get_token() {
+            Ok(value) => { token = value; },
+            Err(err) => {return Err(err);},
+        };
+        while !Self::is_end_token(token) {
+            tokens.push((Self::find_token_name(token), token, lexer.get_yytext(), lexer.start_index(), lexer.end_index()));
+            lexer.advance();
+            match lexer.get_token() {
+                Ok(value) => { token = value; },
+                Err(err) => {return Err(err);},
+            };
+        }
+        Ok(tokens)
+    }
+}
---
+++ src/calc_parser.rs
@@ -0,0 +1,242 @@
+extern crate rustc_serialize;
+
+use self::rustc_serialize::json::Json;
+
+use calc_lexer::CalcLexer;
+use calc_lexer::CalcLexerError;
+
+
+extern crate libc;
+use std;
+
+#[repr(C)]
+#[derive(Debug)]
+pub enum CalcNodeType {
+    Add = 0,
+    Sub = 1,
+    Mul = 2,
+    Div = 3,
+    Minus = 4,
+    Num = 5,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct CalcNode {
+    node_type: CalcNodeType,
+    left: *mut CalcNode,
+    right: *mut CalcNode,
+    str_val: *mut u8,
+}
+
+impl CalcNode {
+    fn new_ptr() -> *mut CalcNode {
+        unsafe {
+            let p: *mut libc::c_void = libc::calloc(1, std::mem::size_of::<CalcNode>());
+            let node: &mut CalcNode = std::mem::transmute(p);
+            node
+        }
+    }
+
+    pub fn free_ptr(p: *mut CalcNode) {
+        unsafe {
+            let node = &mut *p;
+            if !node.left.is_null() {
+                Self::free_ptr(node.left);
+            }
+            if !node.right.is_null() {
+                Self::free_ptr(node.right);
+            }
+            if !node.str_val.is_null() {
+                libc::free(node.str_val as *mut _);
+            }
+            libc::free(node as *mut CalcNode as *mut _);
+        }
+    }
+
+    fn new_node1(node_type: CalcNodeType, left: Json, right: Json) -> Option<Json>
+    {
+        let node = Self::new_ptr();
+        unsafe {
+            (*node).node_type = node_type;
+            (*node).left = std::mem::transmute(left.as_u64().unwrap());
+            (*node).right = std::mem::transmute(right.as_u64().unwrap());
+        }
+        return Some(Json::U64(node as u64));
+    }
+}
+
+
+type Token = usize;
+type State = usize;
+type ProdIndex = usize;
+
+#[derive(Debug)]
+struct ProductionItem {
+    header_id: usize,
+    body_length: usize,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Action {
+    Reduce(ProdIndex),
+    Shift(State),
+    Accept,
+    Goto(State),
+    Error,
+}
+
+#[derive(Debug)]
+pub struct CalcParser {
+    prod_list: Vec<ProductionItem>,
+    lookup_table: Vec<Action>,
+    lookup_index: Vec<usize>,
+}
+
+impl CalcParser {
+    pub fn new() -> CalcParser {
+        CalcParser { prod_list: vec![ProductionItem{header_id: 10, body_length: 2}, ProductionItem{header_id: 9, body_length: 3}, ProductionItem{header_id: 9, body_length: 3}, ProductionItem{header_id: 9, body_length: 3}, ProductionItem{header_id: 9, body_length: 3}, ProductionItem{header_id: 9, body_length: 2}, ProductionItem{header_id: 9, body_length: 3}, ProductionItem{header_id: 9, body_length: 1}]
+               , lookup_index: vec![3, 4, 5, 11, 12, 13, 14, 15, 16, 17, 18, 23, 24, 25, 26, 35, 37, 38, 43, 44, 45, 46, 47, 51, 52, 53, 54, 59, 60, 61, 62, 63, 67, 68, 69, 70, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 99, 100, 101, 102, 107, 108, 109, 110, 111, 112, 113, 114, 119, 120, 121, 122, 144, 145, 146, 151, 152, 153, 154]
+               , lookup_table: vec![Action::Reduce(7), Action::Accept, Action::Reduce(5), Action::Reduce(6), Action::Reduce(1), Action::Reduce(2), Action::Reduce(3), Action::Reduce(4), Action::Shift(2), Action::Shift(2), Action::Shift(2), Action::Shift(2), Action::Shift(2), Action::Shift(2), Action::Shift(2), Action::Reduce(7), Action::Reduce(5), Action::Shift(11), Action::Reduce(6), Action::Reduce(1), Action::Reduce(2), Action::Reduce(3), Action::Reduce(4), Action::Reduce(7), Action::Shift(9), Action::Shift(9), Action::Shift(9), Action::Reduce(6), Action::Shift(9), Action::Shift(9), Action::Reduce(3), Action::Reduce(4), Action::Reduce(7), Action::Shift(7), Action::Reduce(5), Action::Shift(7), Action::Reduce(6), Action::Reduce(1), Action::Reduce(2), Action::Reduce(3), Action::Reduce(4), Action::Shift(1), Action::Shift(1), Action::Shift(1), Action::Reduce(7), Action::Shift(8), Action::Reduce(5), Action::Shift(8), Action::Shift(1), Action::Shift(1), Action::Shift(1), Action::Shift(1), Action::Reduce(6), Action::Reduce(1), Action::Reduce(2), Action::Reduce(3), Action::Reduce(4), Action::Reduce(7), Action::Shift(10), Action::Shift(10), Action::Shift(10), Action::Reduce(6), Action::Shift(10), Action::Shift(10), Action::Reduce(3), Action::Reduce(4), Action::Shift(3), Action::Shift(3), Action::Shift(3), Action::Shift(3), Action::Shift(3), Action::Shift(3), Action::Shift(3), Action::Goto(4), Action::Goto(5), Action::Goto(6), Action::Goto(12), Action::Goto(13), Action::Goto(14), Action::Goto(15)] }
+    }
+
+    fn lookup(&self, token: Token, state: State) -> Action {
+        let input_length: usize = 10;
+		let state_length: usize = 16;
+		if token >= input_length || state >= state_length {
+			return Action::Error;
+		}
+		self.find(token * state_length + state)
+    }
+
+    fn find(&self, value: usize) -> Action {
+        let mut left: usize = 0;
+        let mut right: usize = self.lookup_index.len();
+        let mut old_mid: usize = 0;
+        loop {
+            let mut mid = (left + right) >> 1;
+            let index_value = self.lookup_index[mid];
+            if index_value == value {
+                return self.lookup_table[mid].clone();
+            } else if index_value > value {
+                right = mid;
+            } else {
+                left = mid;
+            }
+
+            if old_mid == mid {
+                return Action::Error;
+            } else {
+                old_mid = mid;
+            }
+        }
+        Action::Error
+    }
+
+    pub fn parse(&self, lexer: &mut CalcLexer) -> Result<Json, CalcLexerError> {
+		
+
+
+        let mut state_stack: Vec<State> = vec![0];
+        let mut output_stack: Vec<Json> = Vec::new();
+
+        loop {
+            let token = match lexer.get_token() {
+                Ok(token) => token,
+                Err(err) => return Err(err),
+            };
+            let state = state_stack[state_stack.len() - 1];
+            let action = self.lookup(token, state);
+            //println!("{:?}", ("cur_state", state, "token", CalcLexer::find_token_name(token), token, lexer.get_yytext(), "action", action));
+            match action {
+                Action::Accept => {
+                    return Ok(output_stack.pop().unwrap());
+                }
+                Action::Shift(next_state) => {
+                    output_stack.push(lexer.get_yytext());
+                    state_stack.push(next_state);
+                    lexer.advance();
+                }
+                Action::Reduce(prod_index) => {
+                    let length = self.prod_list[prod_index].body_length;
+                    let mut _result: Option<Json> = None;
+                    match prod_index {
+                        0x1 => {
+ _result = CalcNode::new_node1(CalcNodeType::Add, (output_stack[output_stack.len() - 3].clone()), (output_stack[output_stack.len() - 1].clone())); 
+},
+0x2 => {
+ _result = CalcNode::new_node1(CalcNodeType::Sub, (output_stack[output_stack.len() - 3].clone()), (output_stack[output_stack.len() - 1].clone())); 
+},
+0x3 => {
+ _result = CalcNode::new_node1(CalcNodeType::Mul, (output_stack[output_stack.len() - 3].clone()), (output_stack[output_stack.len() - 1].clone())); 
+},
+0x4 => {
+ _result = CalcNode::new_node1(CalcNodeType::Div, (output_stack[output_stack.len() - 3].clone()), (output_stack[output_stack.len() - 1].clone())); 
+},
+0x5 => {
+
+        let node = CalcNode::new_ptr();
+        unsafe {
+            (*node).node_type = CalcNodeType::Minus;
+            (*node).left = std::mem::transmute((output_stack[output_stack.len() - 1].clone()).as_u64().unwrap());
+        };
+        _result = Some(Json::U64(node as u64));
+    
+},
+0x6 => {
+ _result = Some((output_stack[output_stack.len() - 2].clone())); 
+},
+0x7 => {
+
+        let node = CalcNode::new_ptr();
+        unsafe {
+            let bytes: Vec<u8> = Vec::from((output_stack[output_stack.len() - 1].clone()).as_string().unwrap());
+            let s_ptr = libc::calloc(bytes.len() + 1, 1);
+            libc::memcpy(s_ptr, bytes.as_ptr() as *const _, bytes.len());
+            (*node).node_type = CalcNodeType::Num;
+            (*node).str_val = s_ptr as *mut _;
+        }
+        _result = Some(Json::U64(node as u64));
+    
+},
+_ => {},
+                    };
+                    if length > 0 && _result.is_none() {
+                        _result = Some(output_stack[output_stack.len() - length].clone());
+                    }
+                    if _result.is_none() {
+                        _result = Some(Json::Null);
+                    }
+                    {
+                        let mut i = 0;
+                        while i < length {
+                            state_stack.pop();
+                            output_stack.pop();
+                            i += 1;
+                        }
+                    }
+                    let next_state = state_stack[state_stack.len() - 1];
+                    let next_action = self.lookup(self.prod_list[prod_index].header_id, next_state);
+                    match next_action {
+                        Action::Goto(next_state) => {
+                            state_stack.push(next_state);
+                            output_stack.push(_result.unwrap());
+                        }
+                        _ => return Err(lexer.new_lexer_error("Goto Error".to_string())),
+                    };
+                }
+                _ => {
+                    return Err(lexer.new_lexer_error("Parser Error".to_string()));
+                }
+            }
+        }
+        Ok(Json::Null)
+    }
+
+    pub fn parse_str(source: &str) -> Result<Json, CalcLexerError> {
+        let mut lexer = CalcLexer::new();
+        lexer.set_source(source);
+        let mut p = Self::new();
+        p.parse(&mut lexer)
+    }
+}
